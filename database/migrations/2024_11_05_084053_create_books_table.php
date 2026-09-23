19f72fbc0afaf646da6edd4bc4f988ea83f4f4ce
getimad <107067409+getimad@users.noreply.github.com>
2026-09-23 18:54:43 +0000

    fix: Add onDelete('cascade') to category_id foreign key in books table

--- database/migrations/2024_11_05_084053_create_books_table.php
+++ database/migrations/2024_11_05_084053_create_books_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->date("publication_date");
             $table->timestamps();
 
-            $table->foreignId("category_id")->constrained();
+            $table->foreignId("category_id")->constrained()->onDelete("cascade");
         });
     }
 
